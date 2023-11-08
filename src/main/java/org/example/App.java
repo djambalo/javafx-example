8a6bc7836935a21498b177a29f4008c9db5d7551
djambalo <laura_ngandjangola@stu.indianhills.edu>
2023-11-07 20:47:21 -0600

    make spme changes

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,11 +9,17 @@ import javafx.event.EventHandler;
 import javafx.geometry.Bounds;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -25,15 +31,38 @@ public class App extends Application {
 
     private final static int SCENE_HEIGHT = 500;
     private final static int SCENE_WIDTH = 500;
+    private int colorIndex = 0;
+    private Color[] colors = {Color.PURPLE, Color.YELLOWGREEN, Color.YELLOW, Color.RED , Color.PINK, Color.BLUE};
 
     @Override
     public void start(Stage stage) {
 
         Pane pane = new Pane();
         Scene scene = new Scene(pane, SCENE_HEIGHT, SCENE_WIDTH);
-        Circle circle = new Circle(20, Color.RED);
+        Circle circle = new Circle(20, colors[colorIndex]);
         circle.relocate(10,10);
+
+        //Setting the radius of the circle
+        circle.setRadius(25.0f);
+
+        //Setting the color of the circle
+        circle.setFill(Color.YELLOWGREEN);
+
+        //Setting the stroke width of the circle
+        circle.setStrokeWidth(20);
+
+        // Text
+        Text text = new Text("Circle changes colors ONLY when it hit a corner.");
+        text.setFont(Font.font(null, FontWeight.BOLD, 15));
+        text.setFill(Color.BLACK);
+        text.setX(150);
+        text.setY(50);
+
+        stage.setTitle("animation");
+
         pane.getChildren().add(circle);
+
+        pane.getChildren().add(text);
 
         stage.setScene(scene);
         stage.show();
@@ -53,6 +82,7 @@ public class App extends Application {
                     System.out.println("Out of bounds X " + circle.getLayoutX());
 
                     dx = -dx;
+                    changeColor(circle);
                 }
 
                 if(circle.getLayoutY() <= (bounds.getMinY() + circle.getRadius()) ||
@@ -63,6 +93,11 @@ public class App extends Application {
         }));
         timeline.setCycleCount(Timeline.INDEFINITE);
         timeline.play();
+    }
+
+    public void changeColor(Circle circle){
+        colorIndex = (colorIndex + 1) % colors.length;
+        circle.setFill(colors[colorIndex]);
     }
 
     public static void main(String[] args) {
